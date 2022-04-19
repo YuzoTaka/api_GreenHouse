a031ab766f0490c8b02e6ed95ab371b80e227192
YuzoTaka <henrique.takahashi@sptech.school>
2022-04-19 20:30:57 -0300

    api pronta

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE greenhouse;
+
+USE greenhouse;
+
+CREATE TABLE usuario(
+	idPF INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    dtNasc DATE,
+    email VARCHAR(45),
+	senha VARCHAR(45)
+);
+
+-- CREATE TABLE usuarioPJ(
+-- 	idPJ INT PRIMARY KEY AUTO_INCREMENT,
+--     nome VARCHAR(45),
+--     nomeRepresentante VARCHAR(45),
+--     CNPJ CHAR(14),
+--     rua VARCHAR(45),
+--     numero INT,
+--     bairro VARCHAR(45),
+--     email VARCHAR(45)
+-- );
+
+CREATE TABLE dados(
+	idDados INT PRIMARY KEY AUTO_INCREMENT,
+    temperatura DECIMAL(10,2),
+    luminosidade INT,
+    umidade INT,
+    dataHora DATETIME
+);
+
+CREATE TABLE sensor(
+	idSensor INT AUTO_INCREMENT,
+    fkDados INT,
+    PRIMARY KEY(idSensor, fkDados),
+    fkPJ INT,
+    FOREIGN KEY(fkPj) REFERENCES usuarioPJ(idPJ),
+    fkPF INT,
+    FOREIGN KEY(fkPF) REFERENCES usuarioPF(idPF)
+);
+
+DESC usuarioPF;
+DESC usuarioPJ;
+DESC sensor;
+DESC dados;
